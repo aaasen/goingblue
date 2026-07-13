a6a8e41a2231a6ec38540bdce544646fb5d720c4
Lane Aasen <laneaasen@gmail.com>
2026-07-12 22:15:29 -0700

    Set twilio webhook URL

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,10 @@ set -euo pipefail
 INSTANCE_CONNECTION_NAME="goingblue:us-west1:goingblue"
 DB_USER="postgres"
 DB_NAME="goingblue"
+TWILIO_WEBHOOK_URL="https://going.blue/sms"
 
 gcloud run deploy goingblue --project goingblue --source . --region us-west1 \
   --allow-unauthenticated --platform managed \
   --add-cloudsql-instances "$INSTANCE_CONNECTION_NAME" \
-  --set-env-vars "INSTANCE_CONNECTION_NAME=$INSTANCE_CONNECTION_NAME,DB_USER=$DB_USER,DB_NAME=$DB_NAME" \
+  --set-env-vars "INSTANCE_CONNECTION_NAME=$INSTANCE_CONNECTION_NAME,DB_USER=$DB_USER,DB_NAME=$DB_NAME,TWILIO_WEBHOOK_URL=$TWILIO_WEBHOOK_URL" \
   --set-secrets "DB_PASS=DB_PASS:latest,TWILIO_AUTH_TOKEN=TWILIO_AUTH_TOKEN:latest"
